848c36f67317a1529fb8b682564cfbe1279fd91c
Ben Kaan <benkaan001@gmail.com>
2021-10-10 00:45:08 -0500

    engineer testing complete

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require('@jest/globals');
+const Engineer = require ('../lib/Engineer');
+
+test('gets the github account of the engineer', () => {
+    const engineer = new Engineer('engineer', 3, 'engineer@noemail.com', 'benkaan001');
+
+    expect(engineer.github).toEqual(expect.any(String));
+
+});
+
+test('gets the github link of the engineer', () => {
+    const engineer = new Engineer('engineer', 3, 'engineer@noemail.com', 'benkaan001');
+
+    expect(engineer.getGithub()).toEqual('https://github.com/benkaan001');
+});
+
+test ('overrides the role of the engineer', () => {
+    const engineer = new Engineer('engineer', 3, 'engineer@noemail.com', 'benkaan001');
+
+    expect(engineer.getRole()).toEqual(expect.any(String));
+});
+
