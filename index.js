cdc6af4a5d9a2b8201817f97f48aef7666bc2beb
Ben Kaan <benkaan001@gmail.com>
2021-10-15 22:29:53 -0500

    getEmployee inquirer prompts

--- index.js
+++ index.js
@@ -96,28 +96,113 @@ const getManager = () => {
     const {name, id, email, officeNumber} = managerInput;
     const manager = new Manager (name, id, email, officeNumber);
 
-    //test 
+  
 
-    console.log(getManager);
 })
 };
 
-// const getEmployees = () => {
+const teamArry =[];
 
-    
+const getEmployees = () => {
+    inquirer.prompt ([
+        {
+            type:'text',
+            name:'role',
+            message: 'Please select the role of the employee:',
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'text',
+            name: 'name',
+            message: 'Please enter the name of the employee:',
+            validate: nameInfo => {
+                if(nameInfo) {
+                    return true;
+                } else {
+                    console.log ('Please enter a name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'id',
+            message: 'Please enter the ID number of the employee:',
+            validate: idInfo => {
+                if (isNaN(idInfo)) {
+                    console.log ('Please enter a valid numeric value for the ID of the employee!');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'text',
+            name: 'email',
+            message: 'Please enter the email address of the employee:',
+            validate: employeeEmail => {
+                const emailValidate = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(employeeEmail);
+                if(emailValidate) {
+                    return true;
+                } else {
+                    console.log('Please enter an email!');
+                    return false;
+                }
+            }
+        },
 
-//     return inquirer.prompt ([
-//         {
-//             type: 'checkbox',
-//             name: 
-//         }
-//     ])
+        {
+            type: 'input',
+            when: (input) => input.role === 'Intern',
+            name: 'school',
+            message: 'Please enter the school name for the Intern:',
+            validate: schoolInput => {
+                if(schoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a school name!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            when: (input) => input.role === 'Engineer',
+            name: 'github',
+            message: 'Please etner the github user name for the Engineer:',
+            validate: githubInput => {
+                if(githubInput){
+                    return true;
+                } else{
+                    console.log('Please enter a github user name!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to add more employees?',
+            default: false
+        }
+
+        
+    ])
+    .then (userInput4Employees => {
+        let {role, name, id, email, school, github, confirmAddEmployee} = userInput4Employees;
+
+        if (role === 'Intern') {
+            employee = new Intern (name, id, email, school);
+        } else if ( role === 'Engineer') {
+            employee = new Engineer (name,id, email, github)
+        }
+        
+        
+    })
+}
 
 
-
-
-
-
-
-// }
 
