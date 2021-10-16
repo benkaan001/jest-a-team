282b4c21b1a14afea730b050b429d0f778374c38
Ben Kaan <benkaan001@gmail.com>
2021-10-16 00:04:24 -0500

    add engin and intern bootstrp cards

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -20,7 +20,42 @@ const managerCard = (manager) => {
    
 };
 
-const engineerCard = (engineer) => {}
+const engineerCard = (engineer) => {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${engineer.name}</h5>
+      <p class="card-text"> This is our intern ${engineer.name}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"> ID: ${engineer.id}</li>
+      <li class="list-group-item"> GitHub: ${engineer.github}</li>
+      <li class="list-group-item"> Email: ${engineer.email}</li>
+    </ul>
+   
+  </div>
+    
+    `;
+};
+
+const internCard = (intern) => {
+    return `
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${intern.name}</h5>
+      <p class="card-text"> This is our intern ${intern.name}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item"> ID: ${intern.id}</li>
+      <li class="list-group-item"> School: ${intern.school}</li>
+      <li class="list-group-item"> Email: ${intern.email}</li>
+    </ul>
+   
+  </div>
+    
+    
+    `
+};
 
 generateHTML = () => {}
 
